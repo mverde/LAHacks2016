e89e6e6ac19db4d8576192cd0eecc36fbbb575b3
Desiree <desireelenart@gmail.com>
2016-04-30 03:08:39 -0700

    Added skybox and spawn shots

---
+++ UnityProject/Assets/Scripts/Characters/Shooter.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shooter : MonoBehaviour {
+
+	public GameObject shot;
+
+	public void Shoot()
+	{
+		Instantiate (shot, transform.position, transform.rotation);
+	}
+}
